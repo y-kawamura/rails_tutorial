d443ecd4921024d24df9dd2149d0a7207697bc74
y-kawamura <tetora.y@gmail.com>
2019-11-09 09:47:31 +0900

    Add secure password to User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", 
+                     email: "user@example.com",
+                     password: "foobar",
+                     password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -65,4 +68,6 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
+
+  
 end
