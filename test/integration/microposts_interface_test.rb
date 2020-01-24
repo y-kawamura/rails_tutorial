1de6e1c86dfb592ff64696265161616487fad643
y-kawamura <tetora.y@gmail.com>
2020-01-24 10:03:42 +0900

    Add test for micropost interface

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "micropost interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    
+    # invalid micropost
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "" } }
+    end
+    assert_select 'div#error_explanation'
+
+    # valid micropost
+    content = "This micropost really ties the room together"
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, params: { micropost: { content: content } }
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+
+    # delete micropost
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+
+    # no delete link when asscess to the other user profile
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
+  end 
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "4 microposts", response.body
+
+    # no micropost user
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
+  end
+end
